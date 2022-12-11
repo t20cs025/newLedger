4e3254b5f5f094266bc9843e9d66acc60b736d33
田口凌 <t20cs025@yamanashi.ac.jp>
2022-12-11 17:09:59 +0900

    [modify?]余計なインポートの削除
    試しでインポートしていたものがエラーを穿いていたので除去しました

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -4,7 +4,7 @@ from .models import Ledger
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
-from .forms import ItemBuy, ItemIdForm, ItemForm,CategoryForm
+from .forms import ItemBuy, ItemIdForm, ItemForm
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from multiprocessing.sharedctypes import template
